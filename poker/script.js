d03f4f9aa0247d6cf7933d78d2cdff1c112e5f8a
Roslj2 <114624943+Roslj2@users.noreply.github.com>
2022-10-08 15:31:43 -0400

    Update script.js

--- poker/script.js
+++ poker/script.js
@@ -16,13 +16,16 @@ const CARD_VALUE_MAP = {
   A: 14
 }
 
-const computerCardSlot = document.querySelector(".computer-card-slot")
-const playerCardSlot = document.querySelector(".player-card-slot")
-const computerDeckElement = document.querySelector(".computer-deck")
-const playerDeckElement = document.querySelector(".player-deck")
+//const computerCardSlot = document.querySelector(".computer-card-slot")
+//const playerCardSlot = document.querySelector(".player-card-slot")
+//const computerDeckElement = document.querySelector(".computer-deck")
+//const playerDeckElement = document.querySelector(".player-deck")
+const pokerCardSlot = document.querySelector("poker-card")
+const pokerDeckElement = docmument.querySelector("poker-deck")
 const text = document.querySelector(".text")
 
-let playerDeck, computerDeck, inRound, stop
+//let playerDeck, computerDeck, inRound, stop
+let pokerDeck, inRound, stop
 
 document.addEventListener("click", () => {
   if (stop) {
@@ -42,9 +45,10 @@ function startGame() {
   const deck = new Deck()
   deck.shuffle()
 
-  const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
-  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
-  computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+  //const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
+  // playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+  // computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+  pokerDeck = new Deck(52)
   inRound = false
   stop = false
 
@@ -53,7 +57,8 @@ function startGame() {
 
 function cleanBeforeRound() {
   inRound = false
-  computerCardSlot.innerHTML = ""
+  //computerCardSlot.innerHTML = ""
+  pokerCardSlot.innerHTML = ""
   playerCardSlot.innerHTML = ""
   text.innerText = ""
 
@@ -64,38 +69,41 @@ function flipCards() {
   inRound = true
 
   const playerCard = playerDeck.pop()
-  const computerCard = computerDeck.pop()
+  const pokerCard = pokerDeck.pop()
+  //const computerCard = computerDeck.pop()
 
   playerCardSlot.appendChild(playerCard.getHTML())
-  computerCardSlot.appendChild(computerCard.getHTML())
+  pokerCardSlot.appendChild(pokerCard.getHTML())
+  //computerCardSlot.appendChild(computerCard.getHTML())
 
   updateDeckCount()
 
-  if (isRoundWinner(playerCard, computerCard)) {
-    text.innerText = "Win"
-    playerDeck.push(playerCard)
-    playerDeck.push(computerCard)
-  } else if (isRoundWinner(computerCard, playerCard)) {
-    text.innerText = "Lose"
-    computerDeck.push(playerCard)
-    computerDeck.push(computerCard)
-  } else {
-    text.innerText = "Draw"
-    playerDeck.push(playerCard)
-    computerDeck.push(computerCard)
-  }
+  //if (isRoundWinner(playerCard, computerCard)) {
+    //text.innerText = "Win"
+    //playerDeck.push(playerCard)
+    //playerDeck.push(computerCard)
+  //} else if (isRoundWinner(computerCard, playerCard)) {
+    //text.innerText = "Lose"
+    //computerDeck.push(playerCard)
+    //computerDeck.push(computerCard)
+  //} else {
+    //text.innerText = "Draw"
+    //playerDeck.push(playerCard)
+    //computerDeck.push(computerCard)
+  //}
 
-  if (isGameOver(playerDeck)) {
-    text.innerText = "You Lose!!"
-    stop = true
-  } else if (isGameOver(computerDeck)) {
-    text.innerText = "You Win!!"
-    stop = true
-  }
+  //if (isGameOver(playerDeck)) {
+    //text.innerText = "You Lose!!"
+    //stop = true
+  //} else if (isGameOver(computerDeck)) {
+    //text.innerText = "You Win!!"
+    //stop = true
+  //}
 }
 
 function updateDeckCount() {
-  computerDeckElement.innerText = computerDeck.numberOfCards
+  //computerDeckElement.innerText = computerDeck.numberOfCards
+  pokerDeckElement.innerText = pokerDeck.numberOfCards
   playerDeckElement.innerText = playerDeck.numberOfCards
 }
 
