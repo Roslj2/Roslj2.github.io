cf496a1ae7a3b081191e9c3bca5b96e5d2e22732
Roslj2 <114624943+Roslj2@users.noreply.github.com>
2022-10-13 19:21:20 -0400

    Update!

--- poker/deck.js
+++ poker/deck.js
@@ -68,11 +68,3 @@ function freshDeck() {
     })
   })
 }
-
-  pokerdeck() {
-    const cardDiv = document.createElement("div")
-    cardDiv.innerText = this.suit
-    cardDiv.classList.add("card", this.color)
-    cardDiv.dataset.value = `${this.value} ${this.suit}`
-    return cardDiv
-  }
--- poker/script.js
+++ poker/script.js
@@ -16,20 +16,28 @@ const CARD_VALUE_MAP = {
   A: 14
 }
 
-//const computerCardSlot = document.querySelector(".computer-card-slot")
-//const playerCardSlot = document.querySelector(".player-card-slot")
-//const computerDeckElement = document.querySelector(".computer-deck")
-//const playerDeckElement = document.querySelector(".player-deck")
-const pokerCardSlot = document.querySelector("poker-card-slot")
-const pokerDeckElement = docmument.querySelector("poker-deck")
+/*
+const computerCardSlot = document.querySelector(".computer-card-slot")
 const playerCardSlot = document.querySelector(".player-card-slot")
-const computerDeckElement = document.querySelector(".player-deck")
+const computerDeckElement = document.querySelector(".computer-deck")
+const playerDeckElement = document.querySelector(".player-deck")
+const text = document.querySelector(".text")
+*/
+
+const dealerCardSlot = document.querySelector(".dealer-deck")
+const cardSlot1 = document.querySelector(".card-slot-1")
+const cardSlot2 = document.querySelector(".card-slot-2")
+const cardSlot3 = document.querySelector(".card-slot-3")
+const cardSlot4 = document.querySelector(".card-slot-4")
+const cardSlot5 = document.querySelector(".card-slot-5")
 const text = document.querySelector(".text")
 
 //let playerDeck, computerDeck, inRound, stop
-let pokerDeck, playerDeck, inRound, stop
+let dealerDeck, inRound, stop
 
 document.addEventListener("click", () => {
+  //text.innerText = "You clicked"
+
   if (stop) {
     startGame()
     return
@@ -43,18 +51,22 @@ document.addEventListener("click", () => {
 })
 
 startGame()
+
 function startGame() {
+
+  text.innerText = "Inside strart game"
+
   const deck = new Deck()
   deck.shuffle()
 
   //const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
-  // playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
-  // computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
-  //inRound = false
-  //stop = false
+  //playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+  //computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
 
-  playerDeck = new Deck()
-  pokerDeck = new Deck()
+  //dealerDeck = new Deck(52)
+  dealerDeck = new Deck()
+  dealerDeck.shuffle()
+
   inRound = false
   stop = false
 
@@ -64,8 +76,13 @@ function startGame() {
 function cleanBeforeRound() {
   inRound = false
   //computerCardSlot.innerHTML = ""
-  pokerCardSlot.innerHTML = ""
-  playerCardSlot.innerHTML = ""
+  //playerCardSlot.innerHTML = ""
+  dealerCardSlot.innerText = ""
+  cardSlot1.innerHTML = ""
+  cardSlot2.innerHTML = ""
+  cardSlot3.innerHTML = ""
+  cardSlot4.innerHTML = ""
+  cardSlot5.innerHTML = ""
   text.innerText = ""
 
   updateDeckCount()
@@ -74,43 +91,110 @@ function cleanBeforeRound() {
 function flipCards() {
   inRound = true
 
-  const playerCard = playerDeck.pop()
-  const pokerCard = pokerDeck.pop()
+  //const playerCard = playerDeck.pop()
   //const computerCard = computerDeck.pop()
 
-  playerCardSlot.appendChild(playerCard.getHTML())
-  pokerCardSlot.appendChild(pokerCard.getHTML())
+  const card1 = dealerDeck.pop()
+  const card2 = dealerDeck.pop()
+  const card3 = dealerDeck.pop()
+  const card4 = dealerDeck.pop()
+  const card5 = dealerDeck.pop()
+
+  //playerCardSlot.appendChild(playerCard.getHTML())
   //computerCardSlot.appendChild(computerCard.getHTML())
+
+  cardSlot1.appendChild(card1.getHTML())
+  cardSlot2.appendChild(card2.getHTML())
+  cardSlot3.appendChild(card3.getHTML())
+  cardSlot4.appendChild(card4.getHTML())
+  cardSlot1.appendChild(card5.getHTML())
 
   updateDeckCount()
 
-  if (isRoundWinner(playerCard, pokerCard)) {
+  isWinningHand(card1, card2, card3, card4, card5)
+
+/*
+  if (isRoundWinner(playerCard, computerCard)) {
     text.innerText = "Win"
     playerDeck.push(playerCard)
-    playerDeck.push(pokerCard)
-  } else if (isRoundWinner(pokerCard, playerCard)) {
+    playerDeck.push(computerCard)
+  } else if (isRoundWinner(computerCard, playerCard)) {
     text.innerText = "Lose"
-    pokerDeck.push(playerCard)
-    pokerDeck.push(pokerCard)
+    computerDeck.push(playerCard)
+    computerDeck.push(computerCard)
   } else {
     text.innerText = "Draw"
     playerDeck.push(playerCard)
-    pokerDeck.push(pokerCard)
+    computerDeck.push(computerCard)
   }
 
   if (isGameOver(playerDeck)) {
     text.innerText = "You Lose!!"
     stop = true
-  } else if (isGameOver(pokerDeck)) {
+  } else if (isGameOver(computerDeck)) {
     text.innerText = "You Win!!"
     stop = true
   }
+
+ */
+}
+
+function isWinningHand(card1, card2, card3, card4, card5) {
+
+  // Load current hand into an array
+  var currentHand = [CARD_VALUE_MAP[card1.value], CARD_VALUE_MAP[card2.value], CARD_VALUE_MAP[card3.value], CARD_VALUE_MAP[card4.value], CARD_VALUE_MAP[card5.value]]
+
+  // Check for 1 pair
+  for (let firstIndex=0; firstIndex < currentHand.length; firstIndex++)
+  {
+    for (let secondIndex=firstIndex+1; secondIndex < currentHand.length; secondIndex++){
+      if (currentHand[firstIndex] == currentHand[secondIndex]){
+        text.innerText = "You have a pair!!"
+      }
+    }
+  }
+
+
+/*
+  if (curentHand[0] == currentHand[1] ||
+    curentHand[0] == currentHand[2] ||
+    curentHand[0] == currentHand[3] ||
+    curentHand[0] == currentHand[4] ||
+    curentHand[0] == currentHand[1])
+
+  // Check for 1 pair
+  if ((CARD_VALUE_MAP[card1.value] == CARD_VALUE_MAP[card2.value])
+    || (CARD_VALUE_MAP[card1.value] == CARD_VALUE_MAP[card3.value])
+    || (CARD_VALUE_MAP[card1.value] == CARD_VALUE_MAP[card3.value])
+    || (CARD_VALUE_MAP[card1.value] == CARD_VALUE_MAP[card4.value])
+    || (CARD_VALUE_MAP[card1.value] == CARD_VALUE_MAP[card5.value])
+    || (CARD_VALUE_MAP[card2.value] == CARD_VALUE_MAP[card3.value])
+    || (CARD_VALUE_MAP[card2.value] == CARD_VALUE_MAP[card4.value])
+    || (CARD_VALUE_MAP[card2.value] == CARD_VALUE_MAP[card5.value])
+    || (CARD_VALUE_MAP[card3.value] == CARD_VALUE_MAP[card4.value])
+    || (CARD_VALUE_MAP[card3.value] == CARD_VALUE_MAP[card5.value])
+    || (CARD_VALUE_MAP[card4.value] == CARD_VALUE_MAP[card5.value]))
+
+ {
+    text.innerText = "You have a pair!"
+  }
+
+*/
+
+  if (dealerDeck.numberOfCards < 5) {
+    stop = true
+    text.innerText = "Game over!"
+  }
+
+
+  // text.innerText = CARD_VALUE_MAP[card1.value]
+
 }
 
 function updateDeckCount() {
   //computerDeckElement.innerText = computerDeck.numberOfCards
-  pokerDeckElement.innerText = pokerDeck.numberOfCards
-  playerDeckElement.innerText = playerDeck.numberOfCards
+  //playerDeckElement.innerText = playerDeck.numberOfCards
+  dealerCardSlot.innerText = dealerDeck.numberOfCards
 }
 
 function isRoundWinner(cardOne, cardTwo) {
@@ -118,5 +202,5 @@ function isRoundWinner(cardOne, cardTwo) {
 }
 
 function isGameOver(deck) {
-  return deck.numberOfCards === 0
+  //return deck.numberOfCards === 0
 }
