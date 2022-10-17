fed6ccfe16dc45311b8c9e7e93ef9f87fdbb5e69
Roslj2 <114624943+Roslj2@users.noreply.github.com>
2022-10-17 18:17:17 -0400

    h

--- poker/script.js
+++ poker/script.js
@@ -237,7 +237,3 @@ function updateDeckCount() {
 function isRoundWinner(cardOne, cardTwo) {
   return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value]
 }
-
-//function isGameOver(deck)
-  //return deck.numberOfCards === 0
-  //
