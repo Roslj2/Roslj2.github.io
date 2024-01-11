be24b1b0d484dcd78ce2348d0075a8787c12d96f
Roslj2 <114624943+Roslj2@users.noreply.github.com>
2024-01-11 10:06:23 -0500

    Update index.js

--- Platformer/index.js
+++ Platformer/index.js
@@ -1,142 +1,142 @@
-import Player from "./player.js"
-import Border from "./border.js"
+import { ctx, rightKey, upKey, downKey, leftKey, borders } from './index.js'
+import checkIntersection from "./index.js"
 
-// Create drawing variables
-var canvas;
-var ctx;
+function Player(x, y) {
+	//Player variable
+	this.x = x;
+	this.y = y;
+	this.xspeed = 0;
+	this.yspeed = 0;
+	this.friction = 0.4;
+	this.maxSpeed = 35;
+	this.width = 100;
+	this.height = 100;
+	this.active = true;
 
-// Create intput variables
-var upKey;
-var rightKey;
-var downKey;
-var leftKey;
+	var horizontalRect
+	var borderRect
 
-// Create game variables
-var gameLoop;
-var player;
-var borders = [];
+	this.step = function() {
+		//Movement
+		if (this.active) {
+			//Horizontal Movement
+				if (!leftKey && !rightKey || leftKey && rightKey) {
+					//Slow down
+					this.xspeed *= this.friction;
+				} else if (rightKey) {
+					//Move right
+					this.xspeed++;
+				} else if (leftKey) {
+					this.xspeed--;
+				}
+			//Vertical Movement
+			if (upKey) {
+				//Check if on the ground
+				this.yspeed -=35;
+			}
 
-// Runs once the page has loaded
-window.onload = function() {
-	// Assign canvas and context variables
-	canvas = document.getElementById("game-canvas");
-	ctx = canvas.getContext("2d");
+			//Apply Gravity
+			this.yspeed += 6;
 
-	//Setup key listeners
-	setupInputs();
+			//Correct Speed
+			if (this.xspeed > this.maxSpeed) {
+				this.xspeed = this.maxSpeed
+			} else if (this.xspeed < -this.maxSpeed) {
+				this.xspeed = -this.maxSpeed
+			}
+			if (this.yspeed > this.maxSpeed) {
+				this.yspeed = this.maxSpeed
+			} else if (this.yspeed < -this.maxSpeed) {
+				this.yspeed = -this.maxSpeed
+			}
+			
 
-	//Create Player
-	player = new Player(450, 0, 100, 100);
-	player.step();
+			if (this.xspeed > 0) {
+				this.xspeed = Math.floor(this.xspeed)
+			} else {
+				this.xspeed = Math.ceil(this.xspeed)
+			}
+			if (this.yspeed > 0) {
+				this.yspeed = Math.floor(this.yspeed)
+			} else {
+				this.yspeed = Math.ceil(this.yspeed)
+			}
 
-	//Create Borders
-	for (let i = 0; i <= 6; i++) {
-		borders.push(new Border(0 + 100 * i, 500, 200, 100, 1));
+			//Horizontal Collision Rect
+			horizontalRect = {
+				x: this.x + this.xspeed,
+				y: this.y,
+				width: this.width,
+				height: this.height
+			}
+
+			//Vertical Collision Rect
+			var verticalRect = {
+				x: this.x,
+				y: this.y + this.yspeed,
+				width: this.width,
+				height: this.height
+			}
+
+			//Check for intersections
+			for (let i = 0; i < borders.length; i++) {
+				borderRect = {
+					x: borders[i].x,
+					y: borders[i].y,
+					width: borders[i].width,
+					height: borders[i].height
+				}
+			}
+
+			this.x += this.xspeed;
+			this.y += this.yspeed;
+
+			//Stops player if on the ground
+			if (this.y >= 450) {
+				this.y = 450;
+			} else if (this.y <= 0) {
+				this.y = 0	
+			}
+
+			if (this.x >= 1000) {
+				this.x = 1000
+			} else if (this.x <= 0) {
+				this.x = 0
+			}
+		} // end ifActive
+
+/*		if (checkIntersection(horizontalRect, borderRect)) {
+			while (checkIntersection(horizontalRect, borderRect)) {
+				horizontalRect.x -= Math.sign(this.xspeed);
+		} 
+*/
+		// The issue is that borderRect is undefined. But, this (playuer) is defined!
+		if (checkIntersection(this, borderRect)) {
+			while (checkIntersection(this, borderRect)) {
+				horizontalRect.x -= Math.sign(this.xspeed);
+		}
+
+		this.xspeed = 0;
+		}
 	}
-	borders.push(new Border(0, 400, 100, 100, 2));
-	for (let i = 0; i < 3; i++) {
-		borders.push(new Border(700, 300 + 100 * i, 100, 200, 2));
+
+	this.draw = function() {
+		ctx.fillStyle = "green";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
 	}
 
-	//Start game loop
-	gameLoop = setInterval(step, 1000 / 30);
-}
+	/*if (checkIntersection(horizontalRect, borderRect)) {
+		//while (checkIntersection(horizontalRect, borderRect)) {
+			//horizontalRect.x -= Math.sign(this.xspeed);
+		//}
+		//this.xspeed = 0;
+		conosle.log("hello");
+	} */
 
-function step() {
-	//Step player
-	player.step();
-
-	//Draw everything
-	draw();
-}
-
-function draw() {
-	//Clear the canvas
-	ctx.fillStyle = "white";
-	ctx.fillRect(0, 0, 1100, 600);
-
-	//Draw the player
-	player.draw();
-
-	//Draw the borders
-	for (let i = 0; i < borders.length; i++) {
-		borders[i].draw();
-	}
-
-}
-
-function setupInputs() {
-	document.addEventListener("keydown", function(event) {
-		if (event.key === "w" || event.key === "ArrowUp") {
-			upKey = true;
-		}
-		else if (event.key === "a" || event.key === "ArrowLeft") {
-			leftKey = true;
-		}
-		else if (event.key === "s" || event.key === "ArrowDown") {
-			downKey = true;
-		}
-		else if (event.key === "d" || event.key === "ArrowRight") {
-			rightKey = true;
-		}
-
-	});
-
-		document.addEventListener("keyup", function(event) {
-		if (event.key === "w" || event.key === "ArrowUp") {
-			upKey = false;
-		}
-		else if (event.key === "a" || event.key === "ArrowLeft") {
-			leftKey = false;
-		}
-		else if (event.key === "s" || event.key === "ArrowDown") {
-			downKey = false;
-		}
-		else if (event.key === "d" || event.key === "ArrowRight") {
-			rightKey = false;
-		}
 
 	
-	});
 }
 
-function checkIntersection(r1, r2) {
-
-	// BEGIN TESTING
-	//alert("r1.y is: " + r1.y + " and r1.height is: " + r1.height + " and r2.y is: " + r2.y);
-	// END TESTING
-
-	if (r1.x >= r2.x + r2.width) {
-		return false;
-	} else if (r1.x + r1.width <= r2.x) {
-		return false;
-	} else if (r1.y >= r2.y + r2.width) {
-		return false;
-	} else if (r1.y + r1.width <= r2.y) {
-		return false;
-	} else {
-		return true;
-	}
-
-/*
-	if (r1.x >= r2.x + r2.width) {
-		return false;
-	} else if (r1.x + r1.width <= r2.x) {
-		return false;
-	} else if (r1.y >= r2.y + r2.height) {
-		return false;
-	} else if (r1.y + r1.height <= r2.y) {
-		return false;
-	} else {
-		return true;
-	}
-*/
-}
-
-
-
-export default checkIntersection
-
-export { ctx, rightKey, upKey, downKey, leftKey, borders };
-
+// Gets rid of no defult export error. Lets index use Player
+export default Player
 
